a44acc0013ff5d32fd7e5aa3c20d43429641f0d4
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-06-01 02:49:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Name = "AliJumani";
+var count = 0;
+for (var _i = 0, Name_1 = Name; _i < Name_1.length; _i++) {
+    var alphabet = Name_1[_i];
+    count++;
+    console.log("Alphabet ".concat(count, "= ") + alphabet);
+}
+console.log("Total number of alphabets are: " + count);
---
+++ main.ts
@@ -0,0 +1,9 @@
+let Name= "AliJumani";
+let count= 0;
+for(let alphabet of Name){
+    count++
+    console.log(`Alphabet ${count}= `+ alphabet);
+        
+}
+
+console.log("Total number of alphabets are: "+count);
